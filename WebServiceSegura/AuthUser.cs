3778e5bc31fe9d10b4ba6be753bd89df7426b082
SEBASTIAN SERRISUELA <ARX0001414@prosegur.com>
2022-10-12 10:11:44 -0300

    Creada la clase AuthUser y agregada la autenticación al método HelloWorldSeguro.

---
+++ WebServiceSegura/AuthUser.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services.Protocols;
+
+namespace WebServiceSegura
+{
+    public class AuthUser : SoapHeader
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }    
+
+        /// <summary>
+        /// Verifica y valida las credenciales.
+        /// </summary>
+        /// <returns></returns>
+        public  bool IsValid()
+        {
+            return true;
+        }
+
+    }
+}
--- WebServiceSegura/MiServicio.asmx.cs
+++ WebServiceSegura/MiServicio.asmx.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Services;
+using System.Web.Services.Protocols;
 
 namespace WebServiceSegura
 {
@@ -16,11 +17,23 @@ namespace WebServiceSegura
     // [System.Web.Script.Services.ScriptService]
     public class MiServicio : System.Web.Services.WebService
     {
+        /// <summary>
+        /// Almacena las credenciales del usuario.
+        /// </summary>
+        public AuthUser authUser;
 
         [WebMethod(Description = "`Método seguro.")]
+        [SoapHeader("authUser")]
         public string HelloWorldSeguro()
         {
-            return "Hola a todos desde un método que implementa seguridad mediante credenciales enviadas en el header.";
+            if (this.authUser != null && this.authUser.IsValid())
+            {
+                return "Hola a todos desde un método que implementa seguridad mediante credenciales enviadas en el header.";
+            }
+            else
+            {
+                return "Credenciales incorrectas!";
+            }                       
         }
 
         [WebMethod(Description = "`Método sin seguridad.")]
